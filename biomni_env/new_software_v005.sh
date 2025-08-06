b0a0412560d98f2a134e0c78944550b4e734dddc
serena2z <serena2zhang@gmail.com>
2025-08-05 21:47:37 -0700

    set up version 0.0.5

---
+++ biomni_env/new_software_v005.sh
@@ -0,0 +1,8 @@
+# Biomni v0.0.5 - Incremental Software Installation
+# Add any new packages/software introduced in version 0.0.5 below
+pip install --force-reinstall numpy==1.26.4
+pip install transformers sentencepiece langchain-google-genai langchain_ollama langchain_aws mcp
+pip install spatialdata muon squidpy scirpy
+pip install decoupler
+pip install pertpy
+pip install formulaic pydeseq2
