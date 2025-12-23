92870a29d08142cd423e71f65811bc6b25510b61
vladsaveliev <vladislav.sav@gmail.com>
2025-12-24 00:30:58 +0100

    Group pip install in new_software, pin langchain-aws<1.0

--- biomni_env/new_software_v008.sh
+++ biomni_env/new_software_v008.sh
@@ -1,18 +1,24 @@
 # Biomni v0.0.8 - Incremental Software Installation
-# Add any new packages/software introduced in version 0.0.8 below
-pip install transformers sentencepiece langchain-google-genai langchain_ollama mcp
-pip install lazyslide
-pip install "git+https://github.com/YosefLab/popV.git@refs/pull/100/head"
-# sudo apt-get install git-lfs # or brew install git-lfs if you are on macOS
-# git lfs install
-pip install pybiomart
-pip install fair-esm
-pip install transcriptformer
-pip install "zarr>=2.0,<3.0"  # this resolved transcripformer download isses
-pip install arc-state
-pip install nnunet nibabel nilearn
-pip install mi-googlesearch-python
-pip install git+https://github.com/pylabrobot/pylabrobot.git
-pip install weasyprint
-pip install "langchain-core>=1.2.2"
-pip install langchain-aws
+# Single pip install to let resolver handle dependencies together
+
+pip install \
+    transformers \
+    sentencepiece \
+    langchain-google-genai \
+    langchain_ollama \
+    mcp \
+    lazyslide \
+    "git+https://github.com/YosefLab/popV.git@refs/pull/100/head" \
+    uv \
+    pybiomart \
+    fair-esm \
+    transcriptformer \
+    "zarr>=2.0,<3.0" \
+    arc-state \
+    nnunet \
+    nibabel \
+    nilearn \
+    mi-googlesearch-python \
+    "git+https://github.com/pylabrobot/pylabrobot.git" \
+    weasyprint \
+    "langchain-aws<1.0"
